37f2cc194093c92db2cf9f13901ad0fd2af90ffb
sharmadishant04 <105786803+sharmadishant04@users.noreply.github.com>
2022-08-24 09:39:07 +0530

    Create 2.c

---
+++ 2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void cesar(char str[], int shift) {
+  int i = 0;
+
+  while (str[i] != '\0') {
+    if (str[i] >= 'A' && str[i]<= 'Z') {
+        char c = str[i] - 'A';
+        c += shift;
+        c = c % 26;
+        str[i] = c + 'A';
+    }
+    i++;
+  }
+  printf("%s", str);
+}
+
+int main()
+{
+    char str[] = "ABCD";
+    cesar(str, 10);
+    return 0;
+}
